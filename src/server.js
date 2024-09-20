a224e2bb4e1653de80fb776371c4a6795cc22aeb
Jakub Šenkýř <senkyr@spstrutnov.cz>
2024-09-20 13:10:53 +0200

    Reakce serveru na různé URL

--- src/server.js
+++ src/server.js
@@ -8,9 +8,15 @@ const app = express();
 app.use(express.static('./www'));
 
 // funkce obsluhujici HTTP dotazy
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/bye', function(req, res) {
+    res.send('Good bye!');
+});
+
 // spusteni aplikace
-app.listen(8000);
+app.listen(8000, 'localhost', () => {
+    console.log('Server běží na http://localhost:8000...');
+});
