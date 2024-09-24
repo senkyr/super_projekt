0bf0c08073bd09bfb8465b63d2b030cd5189890d
Jakub Šenkýř <senkyr@spstrutnov.cz>
2024-09-24 10:15:44 +0200

    Úprava DB modelu, uživatelé mají ID

--- src/app/models/uzivatelModel.js
+++ src/app/models/uzivatelModel.js
@@ -3,10 +3,29 @@ const jsondb = require('simple-json-db');
 // napojeni na soubor s daty
 const db = new jsondb('../data/uzivatele.json');
 
+// inicializace polozky next_id v prazdne databazi
+if(!db.has('next_id')) {
+    db.set('next_id', 1);
+}
+
 exports.existuje = (jmeno) => {
-    return db.has(jmeno);
+    let data = db.JSON();
+
+    delete data['next_id'];
+
+    for(let id in data) {
+        if(data[id]['jmeno'] == jmeno) {
+            return true;
+        }
+    }
+
+    return false;
 };
 
 exports.pridat = (jmeno, heslo) => {
-    db.set(jmeno, heslo);
+    let next_id = db.get('next_id');
+
+    db.set(next_id, {jmeno, heslo});
+
+    db.set('next_id', next_id + 1);
 };
